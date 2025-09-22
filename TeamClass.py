7c58ee8f13aafd19b23f7a4f09b96f860b42afe6
John Seidl <jseidl2@wgu.edu>
2025-09-22 10:25:28 -0500

    Team ID functiob built

---
+++ TeamClass.py
@@ -0,0 +1,17 @@
+import json 
+import requests 
+
+class Roster():
+    def __init__(self, team=None):
+        self.team = team
+
+
+    def get_team_info(self):
+        pass
+        
+
+    def get_roster(self):
+        pass
+
+    def get_player_ID(self):
+        pass 
